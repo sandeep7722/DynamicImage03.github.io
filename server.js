aad42e95caed970778bc3a1044c6fda3c5ae34d9
SANDEEP KUMAR SAROJ <42493758+sandeep7722@users.noreply.github.com>
2023-09-21 15:14:11 +0530

    Update server.js- update images array

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(express.static('images'));
 app.get('/getRandomImage', (req, res) => {
   // Random image selection logic
   const images = ['image1.png', 'image2.png', 'image3.png', 'image4.png', 'image5.png', 'image6.png', 'image7.png', 'image8.png', 'image9.png', 'image10.png', 
-                  'image11.png', 'image12.png', 'image13.png', 'image14.png', 'image15.png', 'image16.png', 'image17.png', 'image18.png', 'image19.png', 'image20.png', 'image21.png', 'image22.png', 'image23.png', 'image24.png', 'image25.png']; // List of image files
+                  'image11.png', 'image12.png', 'image13.png', 'image14.png', 'image15.png', 'image16.png', 'image17.png', 'image18.png', 'image19.png', 'image20.png', 'image21.png', 'image22.png', 'image23.png', 'image24.png', 'image25.png',
+                 'image26.png', 'image27.png', 'image28.png', 'image29.png', 'image30.png', 'image31.png', 'image32.png', 'image33.png', 'image34.png', 'image35.png', 
+                  'image36.png', 'image37.png', 'image38.png', 'image39.png', 'image40.png', 'image41.png', 'image42.png', 'image43.png', 'image44.png', 'image45.png', 'image46.png', 'image47.png', 'image48.png', 'image49.png', 'image50.png',
+                 'image51.png', 'image52.png', 'image53.png', 'image54.png', 'image55.png', 'image56.png', 'image57.png', 'image58.png', 'image59.png', 'image60.png', 'image61.png', 'image62.png', 'image63.png', 'image64.png', 'image65.png']; // List of image files
   const randomImage = images[Math.floor(Math.random() * images.length)];
   
   // Send the selected image as a response
